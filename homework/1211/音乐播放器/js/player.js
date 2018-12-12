37695dcaa82bf6ffd10cc7dd36f0b0a006f63a0e
zhouxuanyou <1102587121@qq.com>
2018-12-12 15:17:01 +0800

    JavaScripday11音乐播放器

--- homework/1211/音乐播放器/js/player.js
+++ homework/1211/音乐播放器/js/player.js
@@ -49,6 +49,8 @@ window.onload = function () {
     let musiclist = document.querySelectorAll('#playerList li');
     //获取图片元素
     let musicimg = document.querySelector('#musicimg');
+    //获取Body属性
+    let bdy = document.body;
 
     // //获得总时间和当前时间
     // let musictime = audio.duration;
@@ -63,19 +65,20 @@ window.onload = function () {
     let musicpath = ['./video/1.mp3','./video/2.mp3','./video/3.mp3'];
     //定义音乐图片路径
     let musicimgpath = ['./images/musicimg/1.jpg','./images/musicimg/2.jpg','./images/musicimg/3.jpg'];
+    let bgimgpath = ['url(./images/bfimg/pap.er/1.jpg)','url(./images/bfimg/pap.er/2.jpg)','url(./images/bfimg/pap.er/3.jpg)','url(./images/bfimg/pap.er/4.jpg)','url(./images/bfimg/pap.er/5.jpg)','url(./images/bfimg/pap.er/6.jpg)','url(./images/bfimg/pap.er/7.jpg)','url(./images/bfimg/pap.er/8.jpg)','url(./images/bfimg/pap.er/9.jpg)','url(./images/bfimg/pap.er/10.jpg)','url(./images/bfimg/pap.er/11.jpg)','url(./images/bfimg/pap.er/12.jpg)','url(./images/bfimg/pap.er/13.jpg)','url(./images/bfimg/pap.er/14.jpg)','url(./images/bfimg/pap.er/15.jpg)','url(./images/bfimg/pap.er/16.jpg)','url(./images/bfimg/pap.er/17.jpg)','url(./images/bfimg/pap.er/18.jpg)','url(./images/bfimg/pap.er/19.jpg)','url(./images/bfimg/pap.er/20.jpg)','url(./images/bfimg/pap.er/21.jpg)','url(./images/bfimg/pap.er/22.jpg)','url(./images/bfimg/pap.er/23.jpg)','url(./images/bfimg/pap.er/24.jpg)','url(./images/bfimg/pap.er/25.jpg)','url(./images/bfimg/pap.er/26.jpg)','url(./images/bfimg/pap.er/27.jpg)','url(./images/bfimg/pap.er/28.jpg)','url(./images/bfimg/pap.er/29.jpg)','url(./images/bfimg/pap.er/30.jpg)','url(./images/bfimg/pap.er/31.jpg)','url(./images/bfimg/pap.er/32.jpg)','url(./images/bfimg/pap.er/33.jpg)'];
     //定义索引
     let num = 0;
 
     //注册事件
-        //播放展厅事件
+    //播放展厅事件
     play.addEventListener('click',thePlay);
-        //下一曲事件
+    //下一曲事件
     next.addEventListener('click',theNext);
-        //上一曲事件
+    //上一曲事件
     prev.addEventListener('click',thePrev);
-        //停止歌曲事件
+    //停止歌曲事件
     stop.addEventListener('click',theStop);
-        //进度条事件d
+    //进度条事件d
     curProgrees.addEventListener('click',thePro);
 
     //设立标杆
@@ -107,7 +110,7 @@ window.onload = function () {
         //标杆取反
         flag = !flag;
     }
-    
+
     //上下曲功能
     function theNext() {
         // for (let i = 0; i < musicpath.length; i++) {
@@ -128,8 +131,9 @@ window.onload = function () {
         thePlay();
         theChancelist();
         theImg();
-    }//上一曲
+    }
 
+    //上一曲
     function thePrev() {
         // for (let i = 0; i < musicpath.length; i++) {
         //     //将路径赋值给音频元素
@@ -160,7 +164,7 @@ window.onload = function () {
         thePlay();
 
     }
-    
+
     // 进度条模块
     function thePro() {
         // //定义计时器
@@ -220,7 +224,7 @@ window.onload = function () {
             totalTime.innerHTML = minutes + ' : ' + secondes;
         },500)
     }
-    
+
     //播放列表模块
     function theChancelist() {
         //遍历出列表
@@ -250,4 +254,19 @@ window.onload = function () {
         }
         musicimg.src = musicimgpath[num];
     }
+
+    //背景图片切换
+    function theBgimg() {
+        setInterval(function(){
+            num ++;
+
+            //判断索引条件
+            if (num > bgimgpath.length-1 ){
+                num = 0;
+            }
+            bdy.style.backgroundImage =  bgimgpath[num];
+        } ,10000)
+
+    }
+    theBgimg();
 };
