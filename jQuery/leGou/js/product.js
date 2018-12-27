a31bf805594c4357848fd779a7cbcecc7b16366f
zhouxuanyou <1102587121@qq.com>
2018-12-27 18:32:55 +0800

    乐购商城项目day5

--- jQuery/leGou/js/product.js
+++ jQuery/leGou/js/product.js
@@ -33,4 +33,28 @@ $(function () {
         $('.meshoufentitle',this).addClass('none');
         $('.meshoufenDetails',this).addClass('block');
     });
+    //主轮播图
+    $("#bannerBoxs").slider({
+        boxh: 518,//盒子的高度
+        w: 1170,//盒子的宽度
+        h: 480,//图片的高度
+        isShow: true,//是否显示控制器
+        isShowBtn: true,//是否显示左右按钮
+        controltop: 5,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20,//控制按钮宽度
+        controlsH: 20,//控制按钮高度
+        radius: 10,//控制按钮圆角度数
+        controlsColor: "#ccc",//普通控制按钮的颜色
+        controlsCurrentColor: "#ff6600",//当前控制按钮的颜色
+        isShowNum: true //是否显示数字
+    });
+    // 喜欢滑动效果
+    let num = 0;
+    $('.qiehuan').click(function () {
+        num++;
+        num%=3;
+        $('.xihuanbox').animate({
+            "top": -num * 481
+        },500);
+    })
 });
